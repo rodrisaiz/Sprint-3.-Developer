cd394aa2dfd83a47b467d3063b29a48dc374e280
Rodri Saiz <rodrisaiz@icloud.com>
2023-04-15 21:10:19 +0200

    Fully working page with all the functions needed to edit a specific task

--- Developers Team/app/models/Task.php
+++ Developers Team/app/models/Task.php
@@ -2,7 +2,6 @@
 
 class Task extends DB{
 
-    public $id;
 
 
     public function showTasks()
@@ -44,8 +43,6 @@ class Task extends DB{
 
         if($this->emotyInputs() == false){
 
-            //$hola = $title;
-
             return header('Location:/web/createtask?error=emotyInputs&title='.$title.'&description='.$description.'&status='.$status.'');
 
         }else{
@@ -82,6 +79,69 @@ class Task extends DB{
             }
         }
     }
+
+
+    public function showOneTask($taskPosition)
+    {
+
+        $user;
+
+        foreach($this->read() as $user){
+
+            if($user["id_user"] == $_SESSION["id"]){
+
+                return $user["tasks"][$taskPosition];
+                
+            }
+        }
+
+    }
+
+
+    public function updateTask($title, $description, $status, $start_date, $end_date)
+    {
+
+        if($this->emotyInputs() == false){
+
+            return header('Location:/web/taskedit?error=emotyInputs&title='.$title.'&description='.$description.'&status='.$status.'');
+
+        }else{
+
+            $counter = -1 ;
+            $id =  $_SESSION["id"];
+
+            foreach($this->read() as $user){
+
+                if( $user['id_user'] == $id){
+
+                    $counter ++;
+
+                    $decoded_json = $this->read();
+
+                    $taskPosition = $_GET['item'];
+
+                    $task = array(
+
+                        'title' => $title,
+                        'description' => $description,
+                        'status' => $status,
+                        'start_date' => $start_date,
+                        'end_date' => $end_date,
+                    );
+
+                
+                    $decoded_json[$counter]['tasks'][$taskPosition] = $task;
+
+                    $write = $this->write($decoded_json);
+
+                    return header('Location:/web/home');
+                }
+            }
+        }
+    }
+
+
+    
 
 
 
--- Developers Team/config/routes.php
+++ Developers Team/config/routes.php
@@ -10,45 +10,7 @@ $routes = array(
 	'/createtask' => 'task#newTask',
 	'/taskwrite' => 'task#write',
 	'/taskedit' => 'task#edit',
+	'/taskupdate' => 'task#update',
 	
-	
-
-	'/test3' => 'task#index',
-	'/test4' => 'task#check',
-
 );
 
-/*$routes = array(
-
-	'/' => 'login#index',
-	'/loginerror' => 'login#error',
-	'/login' => 'login#login',
-	'/signuphome' => 'signup#index',
-	'/signup' => 'signup#signup',
-	'/home' => 'home#home',
-	'/task' => 'task#taskup',
-	'/taskhome' => 'task#index',
-	'/taskedithome' => 'task#edithome',
-	'/taskedit' => 'task#edit',
-	'/user' => 'user#index',
-	'/modifyuser' => 'user#userModify',
-	'/deleteuser' => 'user#delete',
-	'/deleteuser' => 'user#delete',
-	
-
-// the following are just test
-	'/test' => 'test#index',
-	'/testa' => 'test#check',
-	'/rodri' => 'rodri#index'
-
-
-	home -> show all the tasks of that user 
-
-	Title
-	Description
-	Estatus
-	Start date
-	End date
-
-);
-*/
